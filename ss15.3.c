3f20f1fc42467fab541346f66832d895bf3a3f09
tung1911 <101379740+tung1911@users.noreply.github.com>
2022-03-31 21:34:55 +0700

    Add files via upload

---
+++ ss15.3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+main()
+{
+	incre();
+	incre();
+	incre();
+}
+
+incre()
+{
+	static char var=65;
+	printf("\nThe character stored in var is %c",var++);
+}
