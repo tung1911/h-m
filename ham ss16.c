59f6ca2a8d1ab4cae0b9ac3a80082ea72c28136c
tung1911 <101379740+tung1911@users.noreply.github.com>
2022-03-31 21:11:29 +0700

    Add files via upload

---
+++ ham ss16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main()
+{
+	printf("\nI am in main");
+    italy();
+    brazil();
+    argentina();
+}
+italy()
+{
+	printf("\nI am in italy");
+}
+brazil()
+{
+	printf("\nI am in brazil");
+}
+argentina()
+{
+	printf("\nI am in argentina");
+}
